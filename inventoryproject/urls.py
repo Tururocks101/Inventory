8a1b23424f9333d534e47ee7042c93247bcc97b1
Tururocks101! <truphosa.ambiyo@student.moringaschool.com>
2022-04-11 22:52:10 +0300

    create profile page

--- inventoryproject/urls.py
+++ inventoryproject/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventory.urls') ),
     path('', user_view.register, name='user-register'),
+    path('profile', user_view.profile, name='user-profile'),
     path('login/', auth_views.LoginView.as_view(template_name = 'user/login.html'), name = 'user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'user/logout.html'), name ='user-logout'),
 ]
