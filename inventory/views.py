a4f02c6672b165583f89fb48e42452aca0a7d07c
Tururocks101! <truphosa.ambiyo@student.moringaschool.com>
2022-04-11 22:22:00 +0300

    Add login authorization

--- inventory/views.py
+++ inventory/views.py
@@ -1,15 +1,21 @@
+import django
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
+# Create your views here.
+@login_required(login_url='user-login')
 def index(request):
     return render(request, 'dashboard/index.html')
 
+@login_required(login_url='user-login')
 def staff(request):
     return render(request, 'dashboard/staff.html')   
 
+@login_required(login_url='user-login')
 def product(request):
     return render(request, 'dashboard/product.html')
 
+@login_required(login_url='user-login')
 def order(request):
     return render(request, 'dashboard/order.html')    
