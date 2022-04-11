89b8de9aeb8b413eea0037e5925d91b37a9284bb
Tururocks101! <truphosa.ambiyo@student.moringaschool.com>
2022-04-11 20:51:24 +0300

    Add logout form

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -127,6 +127,8 @@ STATICFILES_DIRS =[
     BASE_DIR / "static"
 ]
 
+LOGIN_REDIRECT_URL ='dashboard-index'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
