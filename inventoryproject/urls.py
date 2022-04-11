181df1c85bbbfb8ae6ef74f9743fa51f3d5d8267
Tururocks101! <truphosa.ambiyo@student.moringaschool.com>
2022-04-11 20:44:18 +0300

    Add logout form

--- inventoryproject/urls.py
+++ inventoryproject/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventory.urls') ),
-    path('register/', user_view.register,name='user-register'),
-    path('login/', auth_views.LoginView(), name = 'user-login'),
-    path('logout/', auth_views.LogoutView(), name = 'user-logout'),
+    path('register/', user_view.register, name='user-register'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'user/login.html'), name = 'user-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'user/logout.html'), name ='user-logout'),
 ]
