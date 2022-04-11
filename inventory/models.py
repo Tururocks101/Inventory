6d2915b3d5dd25267f1ad493a0b9d0839aa5a40c
Tururocks101! <truphosa.ambiyo@student.moringaschool.com>
2022-04-11 17:40:23 +0300

    Add to database

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+CATEGORY = (
+    ('Stationary', 'Stationary'),
+    ('Electronics', 'Electronics'),
+    ('Food', 'Food'),
+
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=30, choices= CATEGORY, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.name}'
